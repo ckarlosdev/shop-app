738b04ead3d93dcc8c5b5e19ff8c300948452dff
Carlos Perez <per.ckarlos@gmail.com>
2025-10-08 13:27:45 -0500

    adding gh pages configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/shop-app/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
